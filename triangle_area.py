3c24c241b41705064416270c06bb5a72d86865a3
daveflcc <143441096+daveflcc@users.noreply.github.com>
2025-02-16 18:17:32 -0500

    Initial commit

---
+++ triangle_area.py
@@ -0,0 +1,79 @@
+# FILE NAME - triangle_area.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter the height: 1
+Enter the base: 1
+
+The area of the triangle is 0.5
+
+'''
+
+
+
+'''
+
+Enter the height: 8
+Enter the base: 4
+
+The area of the triangle is 16.0
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is the flow of the program? Which line of code kicks off the process?
+
+
+
+
+
+2. What was the hardest part of this lab?
+
+
+
+
+
+'''
